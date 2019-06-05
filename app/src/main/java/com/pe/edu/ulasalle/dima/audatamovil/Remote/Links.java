8fdc9350453992d052dcd61255c1be6c19c22533
JefryDomingo <jacunam@ulasalle.edu.pe>
2019-06-05 17:49:30 -0500

    Agregando conexiones

--- app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Remote/Links.java
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Remote/Links.java
@@ -1,5 +1,5 @@
 package com.pe.edu.ulasalle.dima.audatamovil.Remote;
 
 public class Links {
-    String URL = "http://198.168.60.45:8080/Audata/servie/";
+    String URL = "http://192.168.11.68:8185/Audata/service/";
 }
--- app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Service/TtsService.java
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Service/TtsService.java
@@ -1,4 +1,12 @@
 package com.pe.edu.ulasalle.dima.audatamovil.Service;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface TtsService {
+    /*
+    @GET("tts/prueba/{name}")
+    Call getArticles();*/
 }
