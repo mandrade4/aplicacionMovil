f928a82bd8e29ed0a8bca4d27cc49a2c0300bcac
dannydi123 <dannydi010@hotmail.com>
2019-06-10 17:02:56 -0500

    Codeando implementando permisos para permitir el gurdado de los packs

--- app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/MainActivity.java
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/MainActivity.java
@@ -1,5 +1,12 @@
 package com.pe.edu.ulasalle.dima.audatamovil.Controller;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,9 +27,11 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private int STORAGE_PERMISSION_CODE = 1;
 
     EditText edtTts;
     Button btnTts;
+
 
     TtsService ttsService;
 
@@ -43,11 +52,56 @@ public class MainActivity extends AppCompatActivity {
                 if(edtTts.getText().toString() == null || edtTts.getText().toString().trim().length() == 0) {
                     Toast.makeText(MainActivity.this, "Se necesita un Texto", Toast.LENGTH_SHORT).show();
                 } else {
-                    sendText(edtTts.getText().toString());
+                    if(ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(MainActivity.this, "Ya tienes permisos", Toast.LENGTH_SHORT).show();
+                        sendText(edtTts.getText().toString());
+                    } else {
+                        requestStoragePermission();
+                    }
+
                 }
 
             }
         });
+    }
+
+    private void requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Se necsitan permisos")
+                    .setMessage("Se necesita acceder a tus backs")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permiso condedido", Toast.LENGTH_SHORT).show();
+                sendText(edtTts.getText().toString());
+            } else {
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void sendText(String text){
