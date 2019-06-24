8b8ba115c3ed9cb71012bce9f93914f00891fe38
JefryDomingo <jacunam@ulasalle.edu.pe>
2019-06-24 17:39:15 -0500

    Creando nuevos activity's y agregando conversion para mp3 y acc

--- app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/TtsActivity.java
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/TtsActivity.java
@@ -4,11 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.pe.edu.ulasalle.dima.audatamovil.R;
 
 public class TtsActivity extends AppCompatActivity {
+
+    RadioGroup ttsRadioGroup;
+    RadioButton ttsRadioButton;
+    Button btnEnviarTts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +39,25 @@ public class TtsActivity extends AppCompatActivity {
 
         getWindow().setAttributes(params);
 
+        ttsRadioGroup = findViewById(R.id.ttsRadioGroup);
+        btnEnviarTts = findViewById(R.id.btnEnviarTts);
+
+        btnEnviarTts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int radioId = ttsRadioGroup.getCheckedRadioButtonId();
+                ttsRadioButton = findViewById(radioId);
+                Toast.makeText(TtsActivity.this,"Radio seleccionado"+ ttsRadioButton.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void checkButtonTts(View v) {
+        int radioId = ttsRadioGroup.getCheckedRadioButtonId();
+
+        ttsRadioButton = findViewById(radioId);
+
+        Toast.makeText(this, "Radio seleccinado" + ttsRadioButton.getText(),Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Remote/Links.java
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Remote/Links.java
@@ -3,7 +3,7 @@ package com.pe.edu.ulasalle.dima.audatamovil.Remote;
 import com.pe.edu.ulasalle.dima.audatamovil.Service.TtsService;
 
 public class Links {
-    public static final String URL = "http://192.168.11.68:8185/Audata/service/";
+    public static final String URL = "http://192.168.60.182:8185/Audata/service/";
 
     public static TtsService getTtsService(){
         return Connection.getClient(URL).create(TtsService.class);
