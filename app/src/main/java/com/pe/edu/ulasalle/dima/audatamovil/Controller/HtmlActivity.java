80c5d32c57303e925115db65dc0fb6c65ba7ab48
dannydi123 <dannydi010@hotmail.com>
2019-06-25 17:10:29 -0500

    Agregando permisos (una sola vez).

---
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/HtmlActivity.java
@@ -0,0 +1,15 @@
+package com.pe.edu.ulasalle.dima.audatamovil.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pe.edu.ulasalle.dima.audatamovil.R;
+
+public class HtmlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_html);
+    }
+}
