b9c9ca9d21e8a9a40a30a616917dac8e23ac2ee7
JefryDomingo <jacunam@ulasalle.edu.pe>
2019-06-25 12:13:01 -0500

    Añadiendo dos funcionalidad reproduccion de texto para mp3 y aac

--- app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/MainActivity.java
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/MainActivity.java
@@ -4,32 +4,18 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.pe.edu.ulasalle.dima.audatamovil.R;
-import com.pe.edu.ulasalle.dima.audatamovil.Remote.Links;
-import com.pe.edu.ulasalle.dima.audatamovil.Service.TtsService;
-
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private int STORAGE_PERMISSION_CODE = 1;
@@ -37,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
     EditText edtTts;
     Button btnTts;
 
-
-    TtsService ttsService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         edtTts = findViewById(R.id.edtTts);
         btnTts = findViewById(R.id.btnTts);
 
-        ttsService = Links.getTtsService();
 
         btnTts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
                     if(ContextCompat.checkSelfPermission(MainActivity.this,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
                         Toast.makeText(MainActivity.this, "Ya tienes permisos", Toast.LENGTH_SHORT).show();
-                        //sendText(edtTts.getText().toString());
                         Intent i = new Intent(getApplicationContext(), TtsActivity.class);
+                        i.putExtra("text",edtTts.getText().toString());
                         startActivity(i);
                     } else {
                         requestStoragePermission();
@@ -104,56 +87,9 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == STORAGE_PERMISSION_CODE) {
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this, "Permiso condedido", Toast.LENGTH_SHORT).show();
-                //sendText(edtTts.getText().toString());
             } else {
                 Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    public void sendText(String text){
-        Call<ResponseBody> call = ttsService.sendText(text);
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if(response.isSuccessful()){
-                    Toast.makeText(MainActivity.this,"Nombre enviado satisfactoriamente", Toast.LENGTH_SHORT).show();
-                    Log.i("Respuesta to String:", response.body().toString());
-                    try {
-                        saveMp3(response.body().bytes());
-                    } catch (IOException e) {
-                        Log.i("Error audio:", e.toString());
-                    }
-
-                } else {
-                    Toast.makeText(MainActivity.this, "Error conexion con el Servidor", Toast.LENGTH_SHORT).show();
-                    Integer error = response.code();
-                    Toast.makeText(MainActivity.this, "Error " + error, Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Log.e("ERROR: ", t.getMessage());
-            }
-        });
-    }
-
-
-    private void saveMp3(byte[] mp3SoundByteArray) {
-        try {
-            File file = new File(
-                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                    "audio.mp3"
-            );
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(mp3SoundByteArray);
-            fos.close();
-            Log.d("FileSize: ", String.valueOf(file.getTotalSpace()));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            Log.d("Error audio: ", String.valueOf(ex));
-        }
-
     }
 }
--- app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/TtsActivity.java
+++ app/src/main/java/com/pe/edu/ulasalle/dima/audatamovil/Controller/TtsActivity.java
@@ -1,8 +1,10 @@
 package com.pe.edu.ulasalle.dima.audatamovil.Controller;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,12 +14,26 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.pe.edu.ulasalle.dima.audatamovil.R;
+import com.pe.edu.ulasalle.dima.audatamovil.Remote.Links;
+import com.pe.edu.ulasalle.dima.audatamovil.Service.TtsService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TtsActivity extends AppCompatActivity {
 
     RadioGroup ttsRadioGroup;
     RadioButton ttsRadioButton;
     Button btnEnviarTts;
+
+    TtsService ttsService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,22 +58,93 @@ public class TtsActivity extends AppCompatActivity {
         ttsRadioGroup = findViewById(R.id.ttsRadioGroup);
         btnEnviarTts = findViewById(R.id.btnEnviarTts);
 
+        ttsService = Links.getTtsService();
+
         btnEnviarTts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int radioId = ttsRadioGroup.getCheckedRadioButtonId();
                 ttsRadioButton = findViewById(radioId);
+                String text = getIntent().getStringExtra("text");
+                String formato = ttsRadioButton.getText().toString();
+
+                if(formato == null | formato.length() == 0){
+                    Toast.makeText(TtsActivity.this, "Selecciona un formato", Toast.LENGTH_SHORT).show();
+                } else {
+                    sendText(text,formato);
+                }
+                Toast.makeText(TtsActivity.this,"Texto enviado"+ getIntent().getStringExtra("text"),Toast.LENGTH_SHORT).show();
                 Toast.makeText(TtsActivity.this,"Radio seleccionado"+ ttsRadioButton.getText(),Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
-    public void checkButtonTts(View v) {
-        int radioId = ttsRadioGroup.getCheckedRadioButtonId();
+    public void sendText(String text, String formato){
+        final String type = formato;
+        System.out.println(text);
+        Call<ResponseBody> call = ttsService.sendText(text);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful()){
+                    Toast.makeText(TtsActivity.this,"Nombre enviado satisfactoriamente", Toast.LENGTH_SHORT).show();
+                    Log.i("Respuesta to String:", response.body().toString());
+                    try {
+                        saveFileAudio(response.body().bytes(), type);
+                    } catch (IOException e) {
+                        Log.i("Error audio:", e.toString());
+                    }
 
-        ttsRadioButton = findViewById(radioId);
+                } else {
+                    Toast.makeText(TtsActivity.this, "Error conexion con el Servidor", Toast.LENGTH_SHORT).show();
+                    Integer error = response.code();
+                    Toast.makeText(TtsActivity.this, "Error " + error, Toast.LENGTH_SHORT).show();
+                }
+            }
 
-        Toast.makeText(this, "Radio seleccinado" + ttsRadioButton.getText(),Toast.LENGTH_SHORT).show();
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("ERROR: ", t.getMessage());
+            }
+        });
+    }
+
+
+    private void saveFileAudio(byte[] mp3SoundByteArray, String type) {
+        UUID uuid = UUID.randomUUID();
+
+        String uuidStringRandom = uuid.toString();
+
+        if (type.equals("MP3")) {
+            try {
+                File file = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                        uuidStringRandom + ".mp3"
+                );
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(mp3SoundByteArray);
+                fos.close();
+                Log.d("FileSize: ", String.valueOf(file.getTotalSpace()));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                Log.d("Error audio: ", String.valueOf(ex));
+            }
+        } else {
+            try {
+                File file = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                        uuidStringRandom + ".aac"
+                );
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(mp3SoundByteArray);
+                fos.close();
+                Log.d("FileSize: ", String.valueOf(file.getTotalSpace()));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                Log.d("Error audio: ", String.valueOf(ex));
+            }
+        }
+
     }
 }
